3cba5d27dacbba3e99209f443386831461396cc2
aladin <msbih.khalil@gmail.com>
2022-10-14 20:17:57 +0100

    ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -15,71 +15,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                margin: EdgeInsets.only(right: 10),
-                color: Colors.amber,
-              ),
-            ],
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              print("from click button");
+            },
+            child: Text("click"),
+            style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(20), backgroundColor: Colors.red),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
