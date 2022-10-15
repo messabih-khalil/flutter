ad4a25ac083acf6c9527e8f988c97198331d7726
aladin <msbih.khalil@gmail.com>
2022-10-15 12:17:41 +0100

    ListView.builder

--- lib/test.dart
+++ lib/test.dart
@@ -35,14 +35,14 @@ class TestState extends State<TestStatefull> {
         body: Container(
           width: double.infinity,
           margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: ListView.builder(
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
               itemCount: tasks.length,
               itemBuilder: (context, i) {
                 return Container(
                   width: double.infinity,
-                  // height: 20,
                   padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                  margin: EdgeInsets.only(top: 20),
                   color: Colors.green,
                   child: Text("${tasks[i]}"),
                 );
