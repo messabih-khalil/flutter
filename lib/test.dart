763d78774e6e0fa771975923fee8511495eb7f8f
aladin <msbih.khalil@gmail.com>
2022-10-15 12:30:51 +0100

    appBar

--- lib/test.dart
+++ lib/test.dart
@@ -8,45 +8,28 @@ class TestStatefull extends StatefulWidget {
 }
 
 class TestState extends State<TestStatefull> {
-  var counter = 0;
-  String? food = "Pizza";
-  bool? checked = false;
-  List<String> tasks = [
-    "cook",
-    "sport",
-    "read book",
-    "cook",
-    "sport",
-    "read book",
-    "cook",
-    "sport",
-    "read book",
-    "cook",
-    "sport",
-    "read book",
-    "cook",
-    "sport",
-    "read book",
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          width: double.infinity,
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
-              itemCount: tasks.length,
-              itemBuilder: (context, i) {
-                return Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                  color: Colors.green,
-                  child: Text("${tasks[i]}"),
-                );
-              }),
-        ));
+        appBar: AppBar(
+          title: Text("Logo"),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.access_alarm),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ],
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
+        body: Text("appBar"));
   }
 }
